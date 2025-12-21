23cca596efd9f66605b9fa32fcb369383b95acff
Selay <selaytoprak44@gmail.com>
2025-12-21 23:07:22 +0300

    Implement Registrable interface for Student class.

--- src/Student.java
+++ src/Student.java
@@ -1,24 +1,51 @@
+import java.util.ArrayList;
 import java.util.UUID;
 
-public class Student {
+public class Student implements Registrable{
     protected static final double ONE_CREDIT_FEE = 1000.0;
     private String fullName;
     private final UUID id;
-
     public Student(String fullName){
         this.fullName = fullName;
         this.id = UUID.randomUUID();
     }
-
     public String getFullName(){
         return fullName;
     }
-
     public void setFullName(String newFullName){
         this.fullName = newFullName;
     }
-
     public UUID getId(){
         return id;
     }
+    @Override
+    public void joinCourse(Course course, Registration registration) throws Exception {
+        registration.registerStudentToCourse(this,course);
+    }
+    @Override
+    public void dropCourse(Course course, Registration registration) throws Exception {
+        registration.dropStudentFromCourse(this,course);
+    }
+    public ArrayList<Course> getRegisteredCourses(CourseCatalog courseCatalog){
+        return courseCatalog.getStudentRegisteredCourses(this);
+    }
+    public ArrayList<Course> getWaitListedCourses(CourseCatalog courseCatalog){
+        return courseCatalog.getStudentWaitListedCourses(this);
+    }
+    public ArrayList<Course> getAppliedCourses(CourseCatalog courseCatalog){
+        ArrayList<Course> registeredCourses = getRegisteredCourses(courseCatalog);
+        ArrayList<Course> waitListedCourses = getWaitListedCourses(courseCatalog);
+        ArrayList<Course> appliedCourses = new ArrayList<Course>(registeredCourses);
+        appliedCourses.addAll(waitListedCourses);
+        return appliedCourses;
+    }
+
+    public double calculateTuition(CourseCatalog courseCatalog) {
+        double sum = 0.0;
+        for (Course course : getRegisteredCourses(courseCatalog)){
+            sum += course.getCredits() * ONE_CREDIT_FEE;
+        }
+        return sum;
+    }
+
 }
