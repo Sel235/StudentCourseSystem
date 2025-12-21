4b7234b7da33d76c320d627c0398bfa488c79dbe
Selay <selaytoprak44@gmail.com>
2025-12-21 22:54:06 +0300

    Create Registrable interface for course operation.

--- src/Registrable.java
+++ src/Registrable.java
@@ -1,2 +1,4 @@
 public interface Registrable {
+    void joinCourse(Course course, Registration registration) throws Exception;
+    void dropCourse(Course course, Registration registration) throws Exception;
 }
