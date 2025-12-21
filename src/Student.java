92f1c109f0122142b4cc3c5fcc2fb7f3f369b6d6
Selay <selaytoprak44@gmail.com>
2025-12-21 22:44:27 +0300

    Create Student class with basic attributes and constructor.

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,24 @@
+import java.util.UUID;
+
 public class Student {
+    protected static final double ONE_CREDIT_FEE = 1000.0;
+    private String fullName;
+    private final UUID id;
+
+    public Student(String fullName){
+        this.fullName = fullName;
+        this.id = UUID.randomUUID();
+    }
+
+    public String getFullName(){
+        return fullName;
+    }
+
+    public void setFullName(String newFullName){
+        this.fullName = newFullName;
+    }
+
+    public UUID getId(){
+        return id;
+    }
 }
