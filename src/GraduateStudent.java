4ea9e0ca1571c5ea903bbd28573c774dd9c8221b
Selay <selaytoprak44@gmail.com>
2025-12-24 00:28:10 +0300

    Create GraduatenStudent class.

--- src/GraduateStudent.java
+++ src/GraduateStudent.java
@@ -1,2 +1,11 @@
-public class GraduateStudent {
+public class GraduateStudent extends Student{
+    private static final double GRAD_CREDIT_DISCOUNT_FACTOR = 0.6;
+    public GraduateStudent(String fullName) {
+        super(fullName);
+    }
+    @Override
+    public double calculateTuition(CourseCatalog courseCatalog) {
+        double baseTuition = super.calculateTuition(courseCatalog);
+        return baseTuition * GRAD_CREDIT_DISCOUNT_FACTOR;
+    }
 }
