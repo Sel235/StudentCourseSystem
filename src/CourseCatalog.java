edce2d3efb8b2193375f732881d501da4735ae50
Selay <selaytoprak44@gmail.com>
2025-12-30 00:08:16 +0300

    Setup CourseCatalog with addCourse validation and find helper.

--- src/CourseCatalog.java
+++ src/CourseCatalog.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CourseCatalog {
+    private List<Course> courseList;
+
+    public CourseCatalog() {
+        this.courseList = new ArrayList<>();
+    }
+
+    public void addCourse(Course course) throws Exception {
+        if (course == null) {
+            throw new Exception("Can't add a null course.");
+        }
+        for (Course existingCourse : courseList) {
+            if (existingCourse.getCourseName().equalsIgnoreCase(course.getCourseName())) {
+                throw new Exception("Course '" + course.getCourseName() + "' already exists in the catalog.");
+            }
+        }
+        courseList.add(course);
+    }
+
+    public List<Course> getAllCourses() {
+        return courseList;
+    }
+
+    public Course findCourse(String courseName) {
+        for (Course course : courseList) {
+            if (course.getCourseName().equalsIgnoreCase(courseName)) {
+                return course;
+            }
+        }
+        return null;
+    }
 }
